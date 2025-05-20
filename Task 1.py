34f424435e145662bec30e07036f0156af4ade0d
= <=>
2025-05-20 16:22:05 +0530

    Task 1 Completed.

---
+++ Task 1.py
@@ -0,0 +1,23 @@
+'''
+Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+'''
+# Code starts from here
+# Took two numbers as input from the users.
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+Addition = num1 + num2
+Substraction = num1-num2
+Multiplication = num1 * num2
+Division = num1/num2
+print("Addition : ",Addition)
+print("Substraction : ",Substraction)
+print("Multiplication : ",Multiplication)
+print("Division : ",Division)
