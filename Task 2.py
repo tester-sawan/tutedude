f39e623ead29de1abc389a3513c06a4fc7c13b7a
= <=>
2025-05-20 16:44:55 +0530

    Task 2 Completed.

---
+++ Task 2.py
@@ -0,0 +1,13 @@
+'''
+Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
+# Code starts here..
+firstName = input("Enter your first name : ")
+lastName = input("Enter your last name : ")
+fullName = firstName + " "+ lastName+"!"
+print("Hello,",fullName,"Welcome to the Python Program. ")
+# Code ends here
