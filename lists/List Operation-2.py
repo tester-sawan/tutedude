1a040ba2fb1993d553e9bc49011ad45fec8e6c93
tester-sawan <sawankumar.qa@gmail.com>
2025-11-26 15:50:34 +0530

    Update-1

--- lists/List Operation-2.py
+++ lists/List Operation-2.py
@@ -1,1 +1,13 @@
 # List operations - extend, remove, pop
+#1. extend() - Add multiple element in  a list
+fruit = ["apple","Banana"]
+print(fruit)
+fruit.extend(["Grapes","Orange"])
+print(fruit)
+#2. remove() - remove an specified element
+fruit.remove("Orange")
+print(fruit)
+#3. pop() - remove the element based on index position and returns the deleted element.
+removed_element = fruit.pop(2)
+print(fruit)
+print(removed_element)
