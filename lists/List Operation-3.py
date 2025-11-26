9a15a523dff7dd5461fdf7bd855972fb8910e869
tester-sawan <sawankumar.qa@gmail.com>
2025-11-26 16:25:47 +0530

    List Operation-3

---
+++ lists/List Operation-3.py
@@ -0,0 +1,16 @@
+# 4. List operation - reverse, sort, count, membership
+#1. reverse
+number = [1,2,3,4,5,6,7,8]
+number1= [3,4,7,1,4,2,3]
+number.reverse()
+print(number)
+#2. sort() - Default is ascending order
+number1.sort()
+print(number1)
+# Sort in descending order
+number1.sort(reverse=True)
+print(number1)
+#3. count() > return the count of the number available in a list
+print(number1.count(4))
+#4. membership - in , not in
+print(9 in number1)
