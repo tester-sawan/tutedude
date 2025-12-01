38a9ce368859766c6091099f11dc3bc08d5e95b9
tester-sawan <sawankumar.qa@gmail.com>
2025-12-01 22:12:01 +0530

    escape sequence

---
+++ String/escape sequences.py
@@ -0,0 +1,9 @@
+# \n - New line
+print("Hello World! \nWe are learning python programming.")
+# \t - tab in between character or word.
+print("Hello\tWorld")
+# \\ - backslash
+print("Hello\\World!")
+# \' - Insert a ' single quoted in a string.
+print('Welcome to Python\'s programming language.')
+# \" - Insert a " double quoted in a string.
