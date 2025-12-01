9ee9941364b4bec8011cdd2edf258ed2770305d0
tester-sawan <sawankumar.qa@gmail.com>
2025-12-01 21:53:37 +0530

    f-string

---
+++ String/fstring.py
@@ -0,0 +1,6 @@
+name = "Sawan"
+age = 29
+print(f"{name} is {age} year old!.")
+# Use f-string to print the value using some mathematical operation.
+num1,num2,num3 = 10,20,30
+print(f"The sum of all these number is {num1+num2+num3}.")
