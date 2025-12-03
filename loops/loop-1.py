9a6f765cf288e61a03b6a282948616df17f0c4a2
tester-sawan <sawankumar.qa@gmail.com>
2025-12-03 15:53:16 +0530

    loop-1

---
+++ loops/loop-1.py
@@ -0,0 +1,11 @@
+employee_info = ["Sawan Kumar","QA",4082,"Adventus.io","Noida, Uttar Pradesh"]
+print(employee_info) # Print the complete list in console.
+for i in employee_info:
+    print(i)
+# for loop for String
+name = "Sawan Kumar"
+for i in name:
+    print(i)
+# Want to print 1 to 10 - use range function
+for i in range(1,11):
+    print(i)
