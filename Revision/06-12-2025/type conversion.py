11b5b112ec48fccf2ab5a7cc74118473f179fde4
tester-sawan <sawankumar.qa@gmail.com>
2025-12-06 16:30:32 +0530

    revision

---
+++ Revision/06-12-2025/type conversion.py
@@ -0,0 +1,16 @@
+name = "Sawan Kumar"
+age = 29
+comple_number = 10+5j
+rate = 10.56
+status = False
+empty = None
+# Find out the data type
+print(type(name))
+print(type(age))
+print(type(comple_number))
+print(type(rate))
+print((type(status)))
+print((type(empty)))
+# manual conversion of one data type into another.
+age = str(age)
+print(type(age))
